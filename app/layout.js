8ffdf165e532969df52775ff6396d108952ef6c7
yiahmed <yahyahmed28@gmail.com>
2023-05-08 12:25:17 -0700

    init

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+  <div>
+      <Navbar/>
       <body className={inter.className}>{children}</body>
-    </html>
+      <Footer/>
+   </div>
   )
 }
---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-nav-height bg-amber-500'>Navbar</div>
+  )
+}
+
+export default Navbar
